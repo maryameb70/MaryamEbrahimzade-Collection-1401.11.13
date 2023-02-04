a3a5daf694efd79539ae094da25a7e28c3a0e3c7
unknown <ma.ebrahimzadeh@gmail.com>
2023-02-04 11:58:22 +0330

    edit class main

--- src/main/java/ir/mapsa/linkedlist/main/Main.java
+++ src/main/java/ir/mapsa/linkedlist/main/Main.java
@@ -5,16 +5,15 @@ import ir.mapsa.linkedlist.Node;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList list = new Node();
+        LinkedList list = new LinkedList();
         list.appendToTail(1);
         list.appendToTail(2);
         list.appendToTail(3);
         list.appendToTail(4);
         System.out.print("Nodes of singly linked list:");
         list.display();
-        Node node = new Node();
-        node.deleteNode(list.getNode(2), 2);
-        node.deleteNode(list.getNode(1), 3);
+        list.deleteNode(list.getNode(2), 2);
+        list.deleteNode(list.getNode(1), 3);
         System.out.print("Linked list elements after remove:");
         list.display();
     }
